c03af490bbb2215cd3be014aec0411416106eadb
ysyszrj <ysyszrj@163.com>
2016-07-19 16:26:19 +0800

    simple dev demo

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by artemis_zrj on 2016/7/19.
+ */
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: './bin',
+        filename: 'app.bundle.js'
+    }
+};
