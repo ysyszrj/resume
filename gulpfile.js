5fbc0b3d89f2c6a63a384d2e38274e4e12965b9c
ysyszrj <ysyszrj@163.com>
2016-07-19 16:26:19 +0800

    simple dev demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 /**
  * Created by artemis_zrj on 2016/7/18.
  */
+var gulp = require('gulp');
+var webpack = require('webpack-stream')
+gulp.task('default',function(){
+    return gulp.src('src/main.js')
+    .pipe(webpack())
+    .pipe(gulp.dest("dist/"));
+});
